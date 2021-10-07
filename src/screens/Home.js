ae7cd16a9111a19d0bff53e69c8300828dc9f875
psh9408p <psh9408p@naver.com>
2021-10-07 09:28:26 +0900

    MapSize 수정

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,14 @@
 /*global naver*/
 /*global kakao*/
-import { gql, useQuery, useReactiveVar } from "@apollo/client"
-import React, { useEffect, useState } from "react"
-import Current from "../components/Current"
-import SearchBox from "../components/SearchBox"
-import FreeBoard from "../components/FreeBoard"
-import Loading from "../components/Loading"
-import Notice from "../components/Notice"
-import Donation from "../components/Donation"
-import { donationPopVar, noticePopVar } from "../apollo"
+import { gql, useQuery, useReactiveVar } from "@apollo/client";
+import React, { useEffect, useState } from "react";
+import Current from "../components/Current";
+import SearchBox from "../components/SearchBox";
+import FreeBoard from "../components/FreeBoard";
+import Loading from "../components/Loading";
+import Notice from "../components/Notice";
+import Donation from "../components/Donation";
+import { donationPopVar, noticePopVar } from "../apollo";
 // import styled from "styled-components"
 // import $ from "jquery"
 
@@ -25,7 +25,7 @@ const TMounts_QUERY = gql`
       cleanCost
     }
   }
-`
+`;
 
 // 버튼 이동 예시 지점
 // const seoul = new naver.maps.LatLngBounds(
@@ -34,7 +34,7 @@ const TMounts_QUERY = gql`
 // )
 
 // 임시 초기 쓰레기산
-const tmpMountain = new naver.maps.LatLng(36.841805, 127.321792)
+const tmpMountain = new naver.maps.LatLng(36.841805, 127.321792);
 
 // 지도 초기 생성 옵션
 const mapOptions = {
@@ -45,35 +45,42 @@ const mapOptions = {
   mapDataControl: true,
   mapTypeControl: false,
   zoomControl: false,
-}
+};
 
-let map // 지도 넣을 곳
-let marker // 현재위치 마커
-const infoViews = [] // 쓰레기산 마커 모음
-let searchMarker // 검색 마커
+let map; // 지도 넣을 곳
+let marker; // 현재위치 마커
+const infoViews = []; // 쓰레기산 마커 모음
+let searchMarker; // 검색 마커
 
 const Home = () => {
-  const noticePop = useReactiveVar(noticePopVar)
-  const donationPop = useReactiveVar(donationPopVar)
+  const noticePop = useReactiveVar(noticePopVar);
+  const donationPop = useReactiveVar(donationPopVar);
 
-  const [getPosition, setGetPosition] = useState(false)
+  const [getPosition, setGetPosition] = useState(false);
 
-  const { data, loading } = useQuery(TMounts_QUERY)
+  const { data, loading } = useQuery(TMounts_QUERY);
 
   // 마커 생성 함수
   const createMarker = (tMountains) => {
     // 여러개 마커 생성 및 이벤트 등록
     tMountains.forEach((tMountain, index) => {
-      const position = new naver.maps.LatLng(tMountain.latitude, tMountain.longtitude)
+      const position = new naver.maps.LatLng(
+        tMountain.latitude,
+        tMountain.longtitude
+      );
 
       // 정보창 html
       var contentString = [
-        '<div style="width:250px;display:flex;align-items:center;flex-direction:column;padding:15px;line-height:150%;border:none;">',
+        '<div class="map-wrap" >',
         '   <div style="margin-bottom:10px;max-width:220px;">',
-        tMountain.image ? `<img src="${tMountain.image}" height="150" alt="쓰레기 지도" />` : null,
+        tMountain.image
+          ? `<img src="${tMountain.image}" height="150" alt="쓰레기 지도" />`
+          : null,
         "   </div>",
         "   <p>규모 : " +
-          (tMountain.amount ? `${tMountain.amount.toLocaleString("ko-KR")}톤` : `미등록`) +
+          (tMountain.amount
+            ? `${tMountain.amount.toLocaleString("ko-KR")}톤`
+            : `미등록`) +
           "   </p>",
         "   <p>처리 여부 : " + (tMountain.finish ? "완료" : "미완료") + "</p>",
         // "   <p>(처리를 지지합니다! 기능 준비중...)</p>",
@@ -81,70 +88,71 @@ const Home = () => {
         `     <a href="/tmountain/${tMountain.id}" style="border:none;padding:10px 20px;border-radius:5px;background-color:#c8d6e5;font-weight:600;">자세히</a>`,
         "   </div>",
         "</div>",
-      ].join("")
+      ].join("");
 
       // 마커 생성부
       var marker = new naver.maps.Marker({
         map: map,
         position,
         icon: "./img/mountain.svg",
-      })
+      });
 
       // 정보창 생성부
       var infowindow = new naver.maps.InfoWindow({
         content: contentString,
 
         // maxWidth: 300,
-        backgroundColor: "white",
-        borderColor: "grey",
-        borderWidth: 2,
+        // backgroundColor: "white",
+        // borderColor: "grey",
+        // borderWidth: 2,
         anchorSize: new naver.maps.Size(0, 0),
         // anchorSkew: true,
         // anchorColor: "white",
 
         pixelOffset: new naver.maps.Point(0, -10),
-      })
-      infoViews.push({ body: infowindow, display: false })
+      });
+      infoViews.push({ body: infowindow, display: false });
 
       // 마커에 이벤트 등록
       naver.maps.Event.addListener(marker, "click", function (e) {
         // console.log(infowindow, infowindow.getMap())
         if (infowindow.getMap()) {
-          infowindow.close()
-          infoViews[index].display = false
+          infowindow.close();
+          infoViews[index].display = false;
         } else {
           //만약 다른 켜져있는 정보창이 있다면 끄기
-          const viewIndex = infoViews.findIndex((a) => a.display)
+          const viewIndex = infoViews.findIndex((a) => a.display);
           if (viewIndex !== -1) {
-            infoViews[viewIndex].body.setMap(null)
-            infoViews[viewIndex].display = false
+            infoViews[viewIndex].body.setMap(null);
+            infoViews[viewIndex].display = false;
           }
           //클릭한 마커 정보창 켜기
-          infowindow.open(map, marker)
-          infoViews[index].display = true
+          infowindow.open(map, marker);
+          infoViews[index].display = true;
         }
-      })
-    })
-  }
+      });
+    });
+  };
 
   const switchPos = () => {
     if (!getPosition) {
-      setGetPosition(true)
+      setGetPosition(true);
     } else {
-      setGetPosition(false)
+      marker.setMap(null);
+      setGetPosition(false);
     }
-  }
+  };
 
   // 현재위치 이동
   const getCurrentPosition = () => {
-    switchPos()
+    switchPos();
 
     if (navigator.geolocation && !getPosition) {
       /* 위치정보 사용 가능 */
       navigator.geolocation.getCurrentPosition((position) => {
-        const latitude = position.coords.latitude
-        const longitude = position.coords.longitude
-        const latlng = new naver.maps.LatLng(latitude, longitude)
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        const latlng = new naver.maps.LatLng(latitude, longitude);
         // map.setCenter(latlng); // 얻은 좌표를 지도의 중심으로 설정합니다.
         // map.setZoom(18);
         marker = new naver.maps.Marker({
@@ -155,81 +163,119 @@ const Home = () => {
               '<img class="pulse" draggable="false" unselectable="on" src="https://myfirstmap.s3.ap-northeast-2.amazonaws.com/circle.png">',
             anchor: new naver.maps.Point(11, 11),
           },
-        })
-        map.setZoom(14, false)
-        map.panTo(latlng)
-      })
+        });
+        map.setZoom(14, false);
+        map.panTo(latlng);
+      });
     } else {
       /* 위치정보 사용 불가능 */
       // alert("위치 정보가 없습니다.");
-      marker.setMap(null)
+      marker.setMap(null);
     }
-  }
+  };
 
   // 검색 이동
-  let ps = new kakao.maps.services.Places()
+  let ps = new kakao.maps.services.Places();
 
-  const SearchBtn = (e) => {
-    if (e.keyCode === 13) {
-      let content = e.target.value
-      ps.keywordSearch(content, placeSearchCB)
-    }
-  }
+  // 주소 이동
 
-  const placeSearchCB = (data, status) => {
+  let geocoder = new kakao.maps.services.Geocoder();
+
+  const addressCB = (data, status) => {
     if ((status = kakao.maps.services.Status.OK)) {
-      let target = data[0]
-      const lat = target.y
-      const lng = target.x
-      const latlng = new naver.maps.LatLng(lat, lng)
+      let target = data[0];
+      const lat = target.y;
+      const lng = target.x;
+      const latlng = new naver.maps.LatLng(lat, lng);
       // 마커 생성부
       if (searchMarker) {
-        searchMarker.setMap(null)
+        searchMarker.setMap(null);
       }
       searchMarker = new naver.maps.Marker({
         position: latlng,
         map: map,
-      })
+      });
 
-      map.setZoom(15, false)
-      map.panTo(latlng)
+      map.setZoom(15, false);
+      map.panTo(latlng);
     } else {
-      alert("??")
+      alert("??");
     }
-  }
+  };
+  const addressSearchB = (e) => {
+    if (e.keyCode === 13) {
+      let content = e.target.value;
+      geocoder.addressSearch(content, addressCB);
+    }
+  };
+
+  const SearchBtn = (e) => {
+    if (e.keyCode === 13) {
+      let content = e.target.value;
+      ps.keywordSearch(content, placeSearchCB);
+    }
+  };
+
+  const placeSearchCB = (data, status) => {
+    if ((status = kakao.maps.services.Status.OK)) {
+      let target = data[0];
+      const lat = target.y;
+      const lng = target.x;
+      const latlng = new naver.maps.LatLng(lat, lng);
+      // 마커 생성부
+      if (searchMarker) {
+        searchMarker.setMap(null);
+      }
+      searchMarker = new naver.maps.Marker({
+        position: latlng,
+        map: map,
+      });
+
+      map.setZoom(15, false);
+      map.panTo(latlng);
+    } else {
+      alert("??");
+    }
+  };
 
   // 지도 초기화
   useEffect(() => {
-    map = new naver.maps.Map("map", mapOptions) // 지도 생성
-    const mapSize = new naver.maps.Size(window.innerWidth, window.innerHeight - 35)
-    map.setSize(mapSize)
+    map = new naver.maps.Map("map", mapOptions); // 지도 생성
+    const mapSize = new naver.maps.Size(
+      window.innerWidth,
+      window.innerHeight - 35
+    );
+    map.setSize(mapSize);
     // 맵 누르면 마커 정보창 꺼지게
     naver.maps.Event.addListener(map, "click", function () {
-      const viewIndex = infoViews.findIndex((a) => a.display)
+      const viewIndex = infoViews.findIndex((a) => a.display);
       if (viewIndex !== -1) {
-        infoViews[viewIndex].body.setMap(null)
-        infoViews[viewIndex].display = false
+        infoViews[viewIndex].body.setMap(null);
+        infoViews[viewIndex].display = false;
       }
-    })
-  }, [])
+    });
+  }, []);
 
   useEffect(() => {
     if (data) {
-      createMarker(data?.seeTMountains) // 마커 생성
+      createMarker(data?.seeTMountains); // 마커 생성
     }
-  }, [data])
+  }, [data]);
   // console.log(data, loading, error)
   return (
-    <div>
+    <div style={{ overflow: "hidden" }}>
       {noticePop && <Notice />}
       {donationPop && <Donation />}
       {loading && <Loading />}
       <div id="map" />
-      <SearchBox SearchBtn={SearchBtn} />
+      <SearchBox addressSearchB={addressSearchB} SearchBtn={SearchBtn} />
       <FreeBoard mountains={data?.seeTMountains} />
-      <Current getCurrentPosition={getCurrentPosition} getPosition={getPosition} />
+      <Current
+        getCurrentPosition={getCurrentPosition}
+        getPosition={getPosition}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
